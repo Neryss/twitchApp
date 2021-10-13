7466de47bc7fb75dc928b14425ebad10a90acefb
neryss <neryssistos@gmail.com>
2021-10-13 00:01:51 +0000

    init je sais pas ce que je fais

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const axios = require('axios').default;
+
+module.exports = {
+	getAppToken: () => {
+		return new Promise(async (resolve, reject) => {
+			const client_id = process.env["CLIENT_ID"];
+			const client_secret = process.env["CLIENT_SECRET"];
+
+			console.log("Waiting for token...");
+			axios({
+				method: "POST",
+				url: "https://id.twitch.tv/oauth2/token",
+				data: {
+					client_id: client_id,
+					client_secret: client_secret,
+					grant_type: "client_credentials",
+					scopes: ""
+				}
+			}).then((res) => {
+				console.log("token generated : " + JSON.stringify(res.data, null, 4));
+				resolve(res.data);
+			}).catch((err) => {
+				console.error(err);
+				reject(err);
+			});
+		});
+	}
+}
+
+async function main() {
+	const getAppToken = require("./index").getAppToken();
+	console.log("cool");
+	getAppToken;
+}
+
+main();
