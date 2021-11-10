7bada9396b698ab34db968f547ee9b5c12f3e9d8
neryss <neryssistos@gmail.com>
2021-11-10 17:34:21 +0000

    added chatbot here, wip channel points

---
+++ chat_bot.js
@@ -0,0 +1,69 @@
+const	tmi = require("tmi.js");
+require("dotenv").config();
+
+const opts = {
+	identity: {
+		username: process.env.USERNAME,
+		password: process.env.PASSWORD
+	},
+	connection: {reconnect: true},
+	channels: [
+		process.env.CHANNEL_NAME
+	]
+};
+
+const client = new tmi.client(opts);
+client.on("message", onMessageHandler);
+client.on("connected", onConnectedHandler);
+client.connect();
+
+function ft_owo(target, context, msg, self)
+{
+	const owos = [`uwu je suis d'accord ${context["display-name"]}`,
+		"owo",
+		"iwi",
+		"TwT",
+		"OwO",
+		"òwó",
+		"ówò",
+		"¯\\_(ツ)_/¯"];
+	client.say(target, owos[Math.floor(Math.random() * (8))]);
+}
+
+function onMessageHandler(target, context, msg, self) {
+	if (self)
+		return ;
+	const commandName = msg.trim();
+	console.log(context);
+	console.log(msg);
+	if (commandName == "!discord")
+		client.say(target, "Le discord : discord.neryss.pw");
+	else if (commandName == "!gogo")
+		client.say(target, "https://www.twitch.tv/gogolegamerzz");
+	else if (commandName == "!nono")
+		client.say(target, "https://www.instagram.com/stitchosaurus/");
+	else if (commandName == "!erios")
+		client.say(target, "https://www.instagram.com/eri0sluna/");
+	else if (commandName == "owo")
+		ft_owo(target, context, msg, self);
+	else if (commandName == "!reseaux")
+		client.say(target, "https://twitter.com/Neryss002\nhttps://www.tiktok.com/@neryss002");
+	else if (commandName == "!modpack")
+		// client.say(target, "Enigmatica6 :\nhttps://www.curseforge.com/minecraft/modpacks/enigmatica6");
+		// client.say(target, "ATM6 :\nhttps://www.curseforge.com/minecraft/modpacks/all-the-mods-6");
+		client.say(target, "Create Above and Beyond : \n https://www.curseforge.com/minecraft/modpacks/create-above-and-beyond");
+	else if (commandName == "!squad")
+		client.say(target, "https://kadgar.net/live/neryss002/gogolegamerzz");
+}
+
+function	onConnectedHandler(addr, port) {
+	console.log(`Connected to ${addr}:${port}`);
+}
+
+module.exports = {
+	say: (msg) => {
+		return new Promise(async (resolve) => {
+			resolve(await client.say(process.env["CHANNEL_NAME"], msg));
+		});
+	},
+}
--- events/channel_points.js
+++ events/channel_points.js
@@ -15,7 +15,88 @@ module.exports = {
 	handle : (data) => {
 		return new Promise(async (resolve, reject) => {
 			console.log(data);
-			resolve();
+			function completed(status, username) {
+				return new Promise(async (resolve) => {
+					if (status)
+						await require("./channel_points").validateRedemption(data.id, data.reward.id);
+					else {
+						await require("./channel_points").cancelRedemption(data.id, data.reward.id);
+						await require("../chat_bot").say(`${username} Oh oh petit problème, tu es remboursé t'en fais pas !`);
+					}
+					resolve();
+				})
+			}
 		})
+	},
+	createReward: (title, cost) => {
+		return new Promise((resolve, reject) => {
+			axios({
+				method: "POST",
+				url: `https://api.twitch.tv/helix/channel_points/custom_rewards?broadcaster_id=${process.env["TWITCH_CHANNEL_ID"]}`,
+				headers: {
+					"Content-Type": "application/json",
+					"Client-ID": process.env["TWITCH_CLIENT_ID"],
+					Authorization: "Bearer " + global.userToken.access_token,
+				},
+				data: {
+					title: title,
+					cost: cost,
+					is_enabled: false,
+				},
+			})
+				.then((res) => {
+					resolve(res.data);
+				})
+				.catch((error) => {
+					console.error(error);
+					reject(error);
+				});
+		});
+	},
+	validateRedemption: (id, reward_id) => {
+		return new Promise((resolve, reject) => {
+			axios({
+				method: "PATCH",
+				url: `https://api.twitch.tv/helix/channel_points/custom_rewards/redemptions?broadcaster_id=${process.env["TWITCH_CHANNEL_ID"]}&reward_id=${reward_id}&id=${id}`,
+				headers: {
+					"Content-Type": "application/json",
+					"Client-ID": process.env["TWITCH_CLIENT_ID"],
+					Authorization: "Bearer " + global.userToken.access_token,
+				},
+				data: {
+					status: "FULFILLED",
+				},
+			})
+				.then((res) => {
+					resolve(res.data);
+				})
+				.catch((error) => {
+					console.error(error);
+					reject(error);
+				});
+		});
+	},
+	cancelRedemption: (id, reward_id) => {
+		return new Promise((resolve, reject) => {
+			axios({
+				method: "PATCH",
+				url: `https://api.twitch.tv/helix/channel_points/custom_rewards/redemptions?broadcaster_id=${process.env["TWITCH_CHANNEL_ID"]}&reward_id=${reward_id}&id=${id}`,
+				headers: {
+					"Content-Type": "application/json",
+					"Client-ID": process.env["TWITCH_CLIENT_ID"],
+					Authorization: "Bearer " + global.userToken.access_token,
+				},
+				data: {
+					status: "CANCELED",
+				},
+			})
+				.then((res) => {
+					resolve(res.data);
+				})
+				.catch((error) => {
+					console.error(error);
+					reject(error);
+				});
+		});
 	}
 }
