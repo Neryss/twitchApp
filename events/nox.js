23113ea76b09d502e859ee595d6642ff1eacc996
neryss <neryssistos@gmail.com>
2021-11-11 16:26:45 +0000

    fix data issue

--- events/nox.js
+++ events/nox.js
@@ -1,22 +1,40 @@
 const fs = require("fs");
+const { resolve } = require("path");
 
 module.exports = {
 	sendPic: () => {
+		times = 0;
+		parse = 1;
 		return new Promise (async (resolve) => {
-			fs.readFile("./resources/nox.json", async function(err, data) {
+			fs.readFile("./resources/nox.json", async function getPic(err, data) {
 				try {
-					data = JSON.parse(data);
-					console.log(Object.keys(data).length);
+					console.log(data);
+					if (parse)
+					{
+						console.log("Parsing inc");
+						data = JSON.parse(data);
+					}
+					// console.log(Object.keys(data).length);
 					selected = data[[Math.floor(Math.random() * (Object.keys(data).length))]]
+					console.log("selected : ");
 					console.log(selected);
-					force = 0;
-					if (!selected.sent && force)
+					if (!selected.sent)
 					{
 						console.log("Done");
 						selected.sent = true;
 						await require("../chat_bot").say(`Hop cadeau chef !\nhttps://neryss.pw/Nox_stream/${selected.url}`);
-						fs.writeFileSync("./resources/nox.json", JSON.stringify(data, null, 4));
+						if (parse)
+							fs.writeFileSync("./resources/nox.json", JSON.stringify(data, null, 4));
+						// else
+						// 	fs.writeFileSync("./resources/nox.json", data);
 						resolve(0);
+					}
+					else if (times < Object.keys(data).length)
+					{
+						times++;
+						console.log("None found");
+						parse = 0;
+						resolve (getPic(err, data, 0));
 					}
 					else
 					{
