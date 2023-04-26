235f1c4a6abe53376d1dc1dd28fd9229ad11efe0
neryss <neryssistos@gmail.com>
2023-04-26 16:17:57 +0000

    changed nox reward wip

--- events/nox.js
+++ events/nox.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
 const { resolve } = require("path");
+const discord = require('discord.js');
+const fetch = require('node-fetch');
+const formData = require('form-data');
 
 module.exports = {
+	
 	sendPic: () => {
 		parse = 1;
 		tested  = [];
@@ -18,7 +22,19 @@ module.exports = {
 						console.log(selected.sent);
 						selected.sent = true;
 						console.log(selected.sent);
-						await require("../chat_bot").say(`Meow chef !\nhttps://neryss.pw/Nox_stream/${selected.url}`);
+						await require("../chat_bot").say(`Meow chef !`);
+
+						const form = new formData();
+						console.log(`00000 ./resources/photos/${selected.name} 00000`)
+						form.append('file', fs.createReadStream(`./resources/photos/${selected.name}`));
+						url = process.env["DISCORD_WEBHOOK_NOX"];
+
+						await fetch(url, {
+							'method': 'POST',
+							'body': form,
+							headers: form.getHeaders()
+						}).then(res => console.log(res))
+						.catch(err => console.log(err))
 						fs.writeFileSync("./resources/nox.json", JSON.stringify(data, null, 4));
 						resolve(0);
 					}
